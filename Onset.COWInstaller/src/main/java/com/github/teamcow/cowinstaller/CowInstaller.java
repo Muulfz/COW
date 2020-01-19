29a7e8d69c8ab751871bddf46759a08ce4452e34
DasDarki <rageskilln@gmail.com>
2020-01-19 14:13:10 +0100

    creating installer

--- Onset.COWInstaller/src/main/java/com/github/teamcow/cowinstaller/CowInstaller.java
+++ Onset.COWInstaller/src/main/java/com/github/teamcow/cowinstaller/CowInstaller.java
@@ -1,11 +1,19 @@
 package com.github.teamcow.cowinstaller;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -13,7 +21,7 @@ public class CowInstaller {
 
     static final Gson GSON = new Gson();
 
-    public static void main(String[] args) throws URISyntaxException {
+    public static void main(String[] args) throws URISyntaxException, IOException {
         System.out.println("________/\\\\\\\\\\\\\\\\\\_______/\\\\\\\\\\_______/\\\\\\______________/\\\\\\_        \n" +
                 " _____/\\\\\\////////______/\\\\\\///\\\\\\____\\/\\\\\\_____________\\/\\\\\\_       \n" +
                 "  ___/\\\\\\/_____________/\\\\\\/__\\///\\\\\\__\\/\\\\\\_____________\\/\\\\\\_      \n" +
@@ -33,28 +41,74 @@ public class CowInstaller {
         OS os = OS.getOS(null);
         if (os == null) {
             System.out.println("[COW: Installer] ERROR: Current OS is not supported!");
+            waitForExit(scanner);
             return;
         }
         System.out.println("[COW: Installer] Select a path (leave empty for current):");
         String dir = scanner.nextLine();
         if (dir == null || dir.trim().isEmpty()) {
             dir = new File(CowInstaller.class.getProtectionDomain().getCodeSource().getLocation()
-                    .toURI()).getPath();
+                    .toURI()).getParentFile().getPath();
         }
         System.out.println("[COW: Installer] Path selected: " + dir);
         if (!isThisDirServer(dir)) {
             System.out.println("[COW: Installer] ERROR: Could not found a server at the select path!");
+            waitForExit(scanner);
             return;
         }
-        System.out.println("[COW:Installer] Starting... Load UPDATE data...");
+        System.out.println("[COW: Installer] Starting... Load UPDATE data...");
         UpdateData data = UpdateData.download();
         if (data == null) {
             System.out.println("[COW: Installer] ERROR: Could not load UPDATE data!");
+            waitForExit(scanner);
             return;
         }
+
+        System.out.println("[COW: Installer] Downloading Files...");
+        Files.copy(new URL(os == OS.WINDOWS ? data.getWinUrl() : data.getLinUrl()).openStream(), Paths.get(dir + "/cow.zip"));
+
+        System.out.println("[COW: Installer] Install Files...");
+        Zip.extract(dir);
+
+        System.out.println("[COW: Installer] Cleaning up...");
+        try {
+            new File(dir + "/cow.zip").delete();
+        } catch (Exception ex) {
+            //Ignore
+        }
+
+        System.out.println("[COW: Installer] Adjust Server...");
+        JsonParser parser = new JsonParser();
+        JsonObject config = parser.parse(new String(Files.readAllBytes(Paths.get(dir + "/server_config.json")))).getAsJsonObject();
+        JsonArray plugins = config.getAsJsonArray("plugins");
+        if (!contains(plugins, "COW.Runtime"))
+            plugins.add("COW.Runtime");
+        JsonArray packages = config.getAsJsonArray("packages");
+        if (!contains(packages, "cow_lua"))
+            packages.add("cow_lua");
+        String json = new GsonBuilder().setPrettyPrinting().create().toJson(config);
+        Files.write(Paths.get(dir + "/server_config.json"), json.getBytes());
+        System.out.println("[COW: Installer] Finished! COW successfully installed!");
+        waitForExit(scanner);
+    }
+
+    private static boolean contains(JsonArray array, String item) {
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i).getAsString().equals(item)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static void waitForExit(Scanner scanner) {
+        System.out.println();
+        System.out.println("Press enter to exit...");
+        scanner.nextLine();
     }
 
     private static boolean isThisDirServer(String path) {
-        return !new File(path + "/package.json").exists();
+        return new File(path + "/server_config.json").exists();
     }
 }
---
+++ Onset.COWInstaller/src/main/java/com/github/teamcow/cowinstaller/Zip.java
@@ -0,0 +1,44 @@
+package com.github.teamcow.cowinstaller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class Zip {
+
+    public static void extract(String destPath) throws IOException {
+        ZipFile zipFile = new ZipFile(destPath + "/cow.zip");
+        Enumeration<?> enu = zipFile.entries();
+        while (enu.hasMoreElements()) {
+            ZipEntry zipEntry = (ZipEntry) enu.nextElement();
+
+            String name = zipEntry.getName();
+            File file = new File(name);
+            if (name.endsWith("/")) {
+                file.mkdirs();
+                continue;
+            }
+
+            File parent = file.getParentFile();
+            if (parent != null) {
+                parent.mkdirs();
+            }
+
+            InputStream is = zipFile.getInputStream(zipEntry);
+            FileOutputStream fos = new FileOutputStream(file);
+            byte[] bytes = new byte[1024];
+            int length;
+            while ((length = is.read(bytes)) >= 0) {
+                fos.write(bytes, 0, length);
+            }
+            is.close();
+            fos.close();
+
+        }
+        zipFile.close();
+    }
+}
